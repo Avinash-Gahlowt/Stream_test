682e551b681f8dc155998543105c59f403b062d5
Avinash Gahlowt <Avinash.Gahlowt@Mantra.Local>
2023-08-24 09:55:34 +0530

    added server

--- sender/sender.js
+++ sender/sender.js
@@ -1,4 +1,4 @@
-const webSocket = new WebSocket("ws://192.168.10.1:3000")
+const webSocket = new WebSocket("https://stream-test-kappa.vercel.app/:3000")
 
 webSocket.onmessage = (event) => {
     handleSignallingData(JSON.parse(event.data))
